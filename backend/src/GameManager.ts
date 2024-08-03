ca0383555a1230ed240b87aa2d5bc45902981e0b
sachin-malani <sachinmalani32@gmail.com>
2024-08-03 12:00:25 +0530

    chess logic

--- backend/src/GameManager.ts
+++ backend/src/GameManager.ts
@@ -1,5 +1,5 @@
 import { WebSocket } from "ws";
-import { INIT_GAME } from "./messages";
+import { INIT_GAME, MOVE } from "./messages";
 import { Game } from "./Game";
 
 
@@ -15,7 +15,8 @@ export class GameManager {
   }
 
   addUser(socket: WebSocket){
-
+    this.users.push(socket);
+    this.handleMessage(socket);
   }
 
   removeUser(socket: WebSocket){
@@ -34,6 +35,13 @@ export class GameManager {
           }
           else {
             this.pendingUser = socket;
+          }
+        }
+
+        if(message.type === MOVE){
+          const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
+          if(game){
+            game.makeMove(socket, message.move);
           }
         }
     })
